72bf5c2a23e1af14c01c378c0d6a516c1c18ab2a
Ela Wieczorek <elizabeth01wieczorek@gmail.com>
2023-03-27 21:47:32 -0400

    Initial commit

---
+++ app/src/main/java/com/ewbugs/learningbottomnavigationbarkotlin/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.ewbugs.learningbottomnavigationbarkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.commit
+import com.ewbugs.learningbottomnavigationbarkotlin.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        supportFragmentManager.commit {
+            add(R.id.fragment_content, WeightFragment())
+        }
+    }
+}
---
+++ app/src/main/java/com/ewbugs/learningbottomnavigationbarkotlin/WeightFragment.kt
@@ -0,0 +1,20 @@
+package com.ewbugs.learningbottomnavigationbarkotlin
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class WeightFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_weight, container, false)
+    }
+
+
+}
