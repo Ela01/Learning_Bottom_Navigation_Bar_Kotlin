09b4936002036ff5b1274053b8acf410fff7da86
Ela Wieczorek <elizabeth01wieczorek@gmail.com>
2023-03-30 21:17:36 -0400

    bottom nav bar working between two interfaces, weight and temp

--- app/src/main/java/com/ewbugs/learningbottomnavigationbarkotlin/MainActivity.kt
+++ app/src/main/java/com/ewbugs/learningbottomnavigationbarkotlin/MainActivity.kt
@@ -2,22 +2,59 @@ package com.ewbugs.learningbottomnavigationbarkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.commit
 import com.ewbugs.learningbottomnavigationbarkotlin.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListener {
 
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
 
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.bottomNav.setOnItemSelectedListener(this)
+
+//        binding.buttonWeight.setOnClickListener { onWeightClicked() }
+//        binding.buttonTime.setOnClickListener { onTimeClicked() }
+
+    }
+
+    private fun onWeightClicked() {
+
         supportFragmentManager.commit {
-            add(R.id.fragment_content, WeightFragment())
+            //'add' will put one frame layout on top of the other. We don't want this
+            replace(R.id.fragment_content, WeightFragment())
+        }
+
+    }
+
+    private fun onTimeClicked() {
+
+        supportFragmentManager.commit {
+            replace(R.id.fragment_content, TimeFragment())
+        }
+
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.nav_ic_weight){
+            onWeightClicked()
+            return true
+        } else if (item.itemId == R.id.nav_ic_time){
+            onTimeClicked()
+            return true
+        } else {
+            return false
         }
     }
+
 }
---
+++ app/src/main/java/com/ewbugs/learningbottomnavigationbarkotlin/TimeFragment.kt
@@ -0,0 +1,17 @@
+package com.ewbugs.learningbottomnavigationbarkotlin
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+//cycling
+class TimeFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+return inflater.inflate(R.layout.fragment_time, container,false )    }
+
+}
