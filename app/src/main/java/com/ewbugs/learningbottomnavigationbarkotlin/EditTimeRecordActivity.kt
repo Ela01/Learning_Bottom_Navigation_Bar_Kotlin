02000b5aab4013ff38784e282310efe86538be32
Ela Wieczorek <elizabeth01wieczorek@gmail.com>
2023-04-10 14:23:44 -0400

    Time conversions update

---
+++ app/src/main/java/com/ewbugs/learningbottomnavigationbarkotlin/EditTimeRecordActivity.kt
@@ -0,0 +1,20 @@
+package com.ewbugs.learningbottomnavigationbarkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ewbugs.learningbottomnavigationbarkotlin.databinding.ActivityEditTimeRecordBinding
+
+
+class EditTimeRecordActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityEditTimeRecordBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityEditTimeRecordBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val unit = intent.getStringExtra("Unit")
+        title = "$unit Converter"
+    }
+}
--- app/src/main/java/com/ewbugs/learningbottomnavigationbarkotlin/TimeFragment.kt
+++ app/src/main/java/com/ewbugs/learningbottomnavigationbarkotlin/TimeFragment.kt
@@ -1,17 +1,52 @@
 package com.ewbugs.learningbottomnavigationbarkotlin
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.ewbugs.learningbottomnavigationbarkotlin.databinding.FragmentTimeBinding
+
 //cycling
 class TimeFragment : Fragment() {
+
+    private lateinit var binding: FragmentTimeBinding
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-return inflater.inflate(R.layout.fragment_time, container,false )    }
+    ): View {
+
+        binding = FragmentTimeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setUpClickListeners()
+
+
+
+
+    }
+    private fun setUpClickListeners(){
+
+        binding.containerTime1.setOnClickListener { launchTimeRecordScreen("seconds") }
+        binding.containerTime2.setOnClickListener { launchTimeRecordScreen("hours") }
+        binding.containerTime3.setOnClickListener { launchTimeRecordScreen("days") }
+        binding.containerTime4.setOnClickListener { launchTimeRecordScreen("weeks") }
+        binding.containerTime5.setOnClickListener { launchTimeRecordScreen("years") }
+
+
+    }
+
+    private fun launchTimeRecordScreen(unit: String) {
+
+        val intent = Intent(context, EditTimeRecordActivity::class.java)
+        intent.putExtra("Unit", unit)
+        startActivity(intent)
+    }
+
 
 }
