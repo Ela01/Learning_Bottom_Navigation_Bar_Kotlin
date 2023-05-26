626e5614699391766aaaac77af6894b71c2c2e6c
Ela Wieczorek <elizabeth01wieczorek@gmail.com>
2023-05-26 03:17:36 -0400

    Fixed some UI/UX issues. Data updating now works.

--- app/src/main/java/com/ewbugs/learningbottomnavigationbarkotlin/weight/EditWeightRecordActivity.kt
+++ app/src/main/java/com/ewbugs/learningbottomnavigationbarkotlin/weight/EditWeightRecordActivity.kt
@@ -1,7 +1,9 @@
 package com.ewbugs.learningbottomnavigationbarkotlin.weight
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.edit
 import com.ewbugs.learningbottomnavigationbarkotlin.databinding.ActivityEditWeightRecordBinding
 
 class EditWeightRecordActivity : AppCompatActivity() {
@@ -13,7 +15,58 @@ class EditWeightRecordActivity : AppCompatActivity() {
         binding = ActivityEditWeightRecordBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val unit = intent.getStringExtra("Unit")
-        title = "$unit Converter"
+        val measurement = intent.getStringExtra("Measurement")
+        title = "$measurement Record"
+
+        displayRecord(measurement)
+        binding.buttonSave.setOnClickListener {
+            saveRecord(measurement)
+            finish()
+
+        }
+
     }
+
+    private fun displayRecord(measurement: String?) {
+        val weightPreferences = getSharedPreferences("measurement", Context.MODE_PRIVATE)
+
+        binding.editTextRecord.setText(weightPreferences.getString("$measurement Record", null))
+        binding.editTextDate.setText(weightPreferences.getString("$measurement Date", null))
+
+    }
+
+    private fun saveRecord(measurement: String?) {
+        val record = binding.editTextRecord.text.toString()
+        val date = binding.editTextDate.text.toString()
+
+        val weightPreferences = getSharedPreferences("measurement", Context.MODE_PRIVATE)
+        weightPreferences.edit {
+            putString("$measurement Record", record)
+            putString("$measurement Date", date)
+        }
+
+
+//        *** The 3 TYPES OF SHARED PREFERENCES ***
+
+//        val applicationPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+//
+//        applicationPreferences.edit {
+//            putString("Some application data", "Application preference value here")
+//        }
+//
+//        val activityPreferences = getPreferences(Context.MODE_PRIVATE)
+//
+//        activityPreferences.edit {
+//            putInt("Some activity data", 32)
+//        }
+//
+//        val fileNamePreferences = getSharedPreferences("some shared preference file name", Context.MODE_PRIVATE)
+//        fileNamePreferences.edit {
+//            putBoolean("Some preference file name data", false)
+//        }
+
+
+    }
+
+
 }
--- app/src/main/java/com/ewbugs/learningbottomnavigationbarkotlin/weight/WeightFragment.kt
+++ app/src/main/java/com/ewbugs/learningbottomnavigationbarkotlin/weight/WeightFragment.kt
@@ -1,5 +1,6 @@
 package com.ewbugs.learningbottomnavigationbarkotlin.weight
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -25,24 +26,40 @@ class WeightFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setUpClickListeners()
-
-
-
-
+        displayRecords()
     }
+
+    override fun onResume() {
+        super.onResume()
+        displayRecords()
+    }
+
     private fun setUpClickListeners(){
-
-        binding.containerWeight1.setOnClickListener { launchWeightRecordScreen("milligram") }
-        binding.containerWeight2.setOnClickListener { launchWeightRecordScreen("microgram") }
-        binding.containerWeight3.setOnClickListener { launchWeightRecordScreen("gram") }
-        binding.containerWeight4.setOnClickListener { launchWeightRecordScreen("kilogram") }
+        binding.containerWeight1.setOnClickListener { launchWeightRecordScreen("Weight") }
+        binding.containerWeight2.setOnClickListener { launchWeightRecordScreen("Neck Measurement") }
+        binding.containerWeight3.setOnClickListener { launchWeightRecordScreen("Hips Measurement") }
+        binding.containerWeight4.setOnClickListener { launchWeightRecordScreen("Waist Measurement") }
 
     }
 
-    private fun launchWeightRecordScreen(unit: String) {
+    private fun displayRecords() {
+        val weightPreferences = requireContext().getSharedPreferences("measurement", Context.MODE_PRIVATE)
+
+        binding.textViewValue1.text = weightPreferences.getString("Weight Record", null)
+        binding.textViewDate1.text = weightPreferences.getString("Weight Date", null)
+        binding.textViewValue2.text = weightPreferences.getString("Neck Measurement Record", null)
+        binding.textViewDate2.text = weightPreferences.getString("Neck Measurement Date", null)
+        binding.textViewValue3.text = weightPreferences.getString("Hips Measurement Record", null)
+        binding.textViewDate3.text = weightPreferences.getString("Hips Measurement Date", null)
+        binding.textViewValue4.text = weightPreferences.getString("Waist Measurement Record", null)
+        binding.textViewDate4.text = weightPreferences.getString("Waist Measurement Date", null)
+
+    }
+
+    private fun launchWeightRecordScreen(measurement: String) {
 
         val intent = Intent(context, EditWeightRecordActivity::class.java)
-        intent.putExtra("Unit", unit)
+        intent.putExtra("Measurement", measurement)
         startActivity(intent)
     }
 
